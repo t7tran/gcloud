4e3bc8df53e475a8b5c1ceb9ce9b5d92225fed28
Tien Tran <tiendung.tran@gmail.com>
2018-08-12 00:05:41 +1000

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ -f "$SERVICE_ACCOUNT_JSON" ]]; then
+        gcloud auth activate-service-account --key-file=$SERVICE_ACCOUNT_JSON &> /dev/null
+fi
+
+if [[ -f "$BASH_SCRIPT" ]]; then
+        . $BASH_SCRIPT
+else
+        exec "$@"
+fi
