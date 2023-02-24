1d40d82e96a3034ca19bc8bed04d7635c1601401
Tien Tran <tiendung.tran@gmail.com>
2023-02-24 14:24:25 +1100

    Do not suppress gcloud error in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
 if [[ -f "$SERVICE_ACCOUNT_JSON" ]]; then
-        gcloud auth activate-service-account --key-file=$SERVICE_ACCOUNT_JSON &> /dev/null
+        gcloud auth activate-service-account --key-file=$SERVICE_ACCOUNT_JSON >/dev/null
 fi
 
 if [[ -f "$BASH_SCRIPT" ]]; then
